379e364c833e7863d3a3e01b3b8a9be935e7ac8c
AgusNeira <a2002neira@gmail.com>
2021-04-26 00:36:07 -0300

    [ADD] Fast evaluation

--- evaluator.js
+++ evaluator.js
@@ -15,38 +15,38 @@ const { lexer } = require('./lexer.js');
 const { syntax_check } = require('./syntax_check.js');
 const { parse } = require('./parse.js');
 
-function traverse(node) {
-    if (node.type === 'block') {
-        traverse(node.child[0]);
-        node.fn = variables => node.child[0].fn(variables);
-    } else if (node.type === 'unary_operator') {
-        traverse(node.child);
-        
-        if (node.operator === '+')
-            node.fn = vars => node.child.fn(vars);
-        else if (node.operator === '-')
-            node.fn = vars => -node.child.fn(vars);
-    } else if (node.type === 'binary_operator'){
-        traverse(node.left);
-        traverse(node.right);
-        
-        if (node.operator === '+')
-            node.fn = vars => node.left.fn(vars) + node.right.fn(vars);
-        else if (node.operator === '-')
-            node.fn = vars => node.left.fn(vars) - node.right.fn(vars);
-        else if (node.operator === '*')
-            node.fn = vars => node.left.fn(vars) * node.right.fn(vars);
-        else if (node.operator === '/')
-            node.fn = vars => node.left.fn(vars) / node.right.fn(vars);
-        else if (node.operator === '^')
-            node.fn = vars => node.left.fn(vars) ** node.right.fn(vars);
-    } else if (node.type === 'literal') {
-        node.fn = vars => parseInt(node.value, 10);
-    } else if (node.type === 'variable')
-        node.fn = vars => vars[node.name];
-}
-
 function evaluate(expression) {
+    function traverse(node) {
+        if (node.type === 'block') {
+            traverse(node.child[0]);
+            node.fn = variables => node.child[0].fn(variables);
+        } else if (node.type === 'unary_operator') {
+            traverse(node.child);
+            
+            if (node.operator === '+')
+                node.fn = vars => node.child.fn(vars);
+            else if (node.operator === '-')
+                node.fn = vars => -node.child.fn(vars);
+        } else if (node.type === 'binary_operator'){
+            traverse(node.left);
+            traverse(node.right);
+            
+            if (node.operator === '+')
+                node.fn = vars => node.left.fn(vars) + node.right.fn(vars);
+            else if (node.operator === '-')
+                node.fn = vars => node.left.fn(vars) - node.right.fn(vars);
+            else if (node.operator === '*')
+                node.fn = vars => node.left.fn(vars) * node.right.fn(vars);
+            else if (node.operator === '/')
+                node.fn = vars => node.left.fn(vars) / node.right.fn(vars);
+            else if (node.operator === '^')
+                node.fn = vars => node.left.fn(vars) ** node.right.fn(vars);
+        } else if (node.type === 'literal') {
+            node.fn = vars => parseInt(node.value, 10);
+        } else if (node.type === 'variable')
+            node.fn = vars => vars[node.name];
+    }
+
     let [tokens, unknowns] = lexer(expression);
     tokens = syntax_check(tokens);
     let parse_tree = parse(tokens);
@@ -70,7 +70,41 @@ function evaluate(expression) {
     };
 }
 
-module.exports = { evaluate };
+function fastEvaluate(expression, values) {
+    function traverse(node, values) {
+        if (node.type === 'block') return traverse(node.child[0], values);
+        else if (node.type === 'unary_operator') {
+            if (node.operator === '+') return traverse(node.child, values);
+            else if (node.operator === '-') return -traverse(node.child, values);
+        } else if (node.type === 'binary_operator') {
+            if (node.operator === '+')
+                return traverse(node.left, values) + traverse(node.right, values);
+            else if (node.operator === '-')
+                return traverse(node.left, values) - traverse(node.right, values);
+            else if (node.operator === '*')
+                return traverse(node.left, values) * traverse(node.right, values);
+            else if (node.operator === '/')
+                return traverse(node.left, values) / traverse(node.right, values);
+        } else if (node.type === 'literal') 
+            return parseInt(node.value, 10);
+        else if (node.type === 'variable') return values[node.name];
+    }
+
+    let [tokens, unknowns] = lexer(expression);
+    tokens = syntax_check(tokens);
+    let parse_tree = parse(tokens);
+
+    // Checking for correct values passed
+    if (Object.keys(values).length !== unknowns.length)
+        throw Error(`Incorrect number of values passed: should be ${unknowns.length} and is ${Object.keys(values).length}`);
+    for (let v of unknowns)
+        if (!values.hasOwnProperty(v))
+            throw Error(`Missing value for variable ${v}`);
+
+    return traverse(parse_tree, values);
+}
+
+module.exports = { evaluate, fastEvaluate };
 
 if (!module.parent) {
     let str = "((x + 5) / (3 - y))";
@@ -78,16 +112,19 @@ if (!module.parent) {
 
     console.log(`Expression: ${str}`);
     console.log(`Result with x=2 and y=5: ${expression.evaluate({ x: 2, y: 5 })}`);
+    console.log(`Fast evaluation: ${fastEvaluate(str, { x: 2, y: 5 })}`);
 
     str = "(-(-x + 5) / (-3 + y))";
     expression = evaluate(str);
 
     console.log(`Expression: ${str}`);
     console.log(`Result with x=2 and y=5: ${expression.evaluate({ x: 2, y: 5 })}`);
+    console.log(`Fast evaluation: ${fastEvaluate(str, { x: 2, y: 5 })}`);
 
     str = "+4(x + 3x)(-9 - x)";
     expression = evaluate(str);
 
     console.log(`Expression: ${str}`);
     console.log(`Result with x=2: ${expression.evaluate({ x: 2 })}`);
+    console.log(`Fast evaluation: ${fastEvaluate(str, { x: 2})}`);
 }
